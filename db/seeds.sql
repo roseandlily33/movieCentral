df986dc8764d9b025ec929e314d8c678722bc5cf
Victoria Benoit <vrose834@gmail.com>
2023-04-16 20:05:13 -0300

    initalized  repo

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO movies(movie_name)
-VALUES ('Shrek'), ('Shrek 2'), ('Cinderella'), ('Cheaper by the dozen'), ('Cheaper by the dozen 2');
-
-INSERT INTO reviews(movie_id, review)
-VALUES(1, 'Best Movie Ever')
-(2, 'Best Sequel Ever')
-(3, 'Best Disney Princess Movie Ever'),
-(4, 'Best Family Ever'),
-(5, '2nd Best Sequel Ever');
